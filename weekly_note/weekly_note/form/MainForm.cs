b256451f2cb67fa6ec321998db0d2ff98c285dcd
多多 <1092501244@qq.com>
2019-01-04 11:27:19 +0800

    1
    窗口顶部贴附

---
+++ weekly_note/weekly_note/form/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp2;
+
+namespace nncqweekly.form
+{
+    public partial class MainForm : Form
+    {
+        public static MainForm mainForm=null;
+        public static MainPointForm mainPointForm = null;
+        public MainForm()
+        {
+            InitializeComponent();
+            mainForm = this;
+        }
+        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            if (mainPointForm == null)
+            {
+                mainPointForm = new MainPointForm();
+                mainPointForm.ShowDialog();
+            }
+            
+        }
+    }
+}
